618f812a652625483fec6f0016a70fbf1a8fef5c
Tom Marsh <ts.marsh@gmail.com>
2020-05-18 02:28:59 -0400

    First post

---
+++ ggol/chunk/src/main/java/com/tailoredshapes/ggol/Chunk.java
@@ -0,0 +1,7 @@
+package com.tailoredshapes.ggol;
+
+public interface Chunk {
+    static void main(String... args){
+        System.out.println("Chunk");
+    }
+}
---
+++ ggol/edge/src/main/java/com/tailoredshapes/ggol/Edge.java
@@ -0,0 +1,7 @@
+package com.tailoredshapes.ggol;
+
+public interface Edge {
+    static void main(String... args){
+        System.out.println("EDGE");
+    }
+}
